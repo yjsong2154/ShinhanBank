db5bd8fe9b75a591abb9a983c22dd48946d3ed85
yjsong2154 <yjsong2153@gmail.com>
2025-08-13 13:12:21 +0900

    Chore : make folder

---
+++ shinhanbank-front/src/styles/theme.ts
@@ -0,0 +1,20 @@
+import type { DefaultTheme } from 'styled-components';
+
+export const theme: DefaultTheme = {
+  colors: {
+    primary: '#6a4ff2',
+    secondary: '#f7f6ff',
+    text: '#333333',
+    lightGray: '#aaaaaa',
+    white: '#ffffff',
+    black: '#000000',
+  },
+  fonts: {
+    main: 'Arial, sans-serif',
+  },
+  spacing: {
+    small: '8px',
+    medium: '16px',
+    large: '24px',
+  },
+};
