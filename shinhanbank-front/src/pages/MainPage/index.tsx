7ae8bf018ed1a6c7092ee87f4b74998dc53fb17b
YeRimmm-Cho <rim000514@gmail.com>
2025-08-18 21:43:00 +0900

    Style(main page): challenge-card UI polishing

--- shinhanbank-front/src/pages/MainPage/index.tsx
+++ shinhanbank-front/src/pages/MainPage/index.tsx
@@ -1,23 +1,23 @@
 // import { useNavigate } from 'react-router-dom'; //저축통 버튼 관련
-import * as S from './MainPage.styles'
-import ChallengeCard from './components/ChallengeCard/ChallengeCard';
-import MySavingsStatus from './components/MySavingsStatus/MySavingsStatus';
-import RankingSection from './components/Ranking/RankingSection';
-import FeedSection from './components/Feed/FeedSection';
+import * as S from "./MainPage.styles";
+import ChallengeCard from "./components/ChallengeCard/ChallengeCard";
+import MySavingsStatus from "./components/MySavingsStatus/MySavingsStatus";
+import RankingSection from "./components/Ranking/RankingSection";
+import FeedSection from "./components/Feed/FeedSection";
 
 const MainPage = () => {
   // const navigate = useNavigate(); //저축통 버튼 관련
 
   const handleCreateSavings = () => {
     // TODO: '새 저축통 만들기' 페이지로 이동하는 로직을 여기에 추가합니다.
-    console.log('새 저축통 만들기 버튼 클릭됨');
+    console.log("새 저축통 만들기 버튼 클릭됨");
     // navigate('/create-savings');
   };
 
   return (
     <S.Container>
       <S.Header>
-        <S.Title>저축 챌린지</S.Title>
+        <S.Title>적금통 키우기</S.Title>
         {/* TODO: 알림 아이콘, 프로필 이미지 컴포넌트 추가 */}
       </S.Header>
 
