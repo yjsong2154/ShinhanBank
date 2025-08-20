d565ce301a5bce729e6efef8fd49e6c36cad795f
조예림 <144078388+YeRimmm-Cho@users.noreply.github.com>
2025-08-20 11:17:17 +0900

    Fix : FeedSection UI polishing (#2)
    * Style(main page): challenge-card UI polishing
    
    * Fix: FeedSection UI polishing

--- shinhanbank-front/src/pages/MainPage/components/Feed/FeedItem.tsx
+++ shinhanbank-front/src/pages/MainPage/components/Feed/FeedItem.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
-import * as S from './FeedItem.styles';
+import React from "react";
+import * as S from "./FeedItem.styles";
+import Character from "../../../../components/Character/Character";
 // import heartIcon from '../../assets/icons/heart.svg';
 // import commentIcon from '../../assets/icons/comment.svg';
 
@@ -28,25 +29,37 @@ const FeedItem: React.FC<FeedItemProps> = ({
 
   return (
     <S.Container>
-      <S.AuthorInfo>
-        <S.Author>{author}</S.Author>
-        <S.Period>{period}</S.Period>
-      </S.AuthorInfo>
-      <S.Title>{title}</S.Title>
-      <S.Content>{content}</S.Content>
-      <S.ProgressInfo>
-        <span>목표: ₩{target.toLocaleString()} ({progressPercentage}%)</span>
-      </S.ProgressInfo>
-      <S.ReactionInfo>
-        <S.ReactionItem>
-          {/* <S.ReactionIcon src={heartIcon} alt="좋아요" /> */}
-          <S.ReactionCount>{likes}</S.ReactionCount>
-        </S.ReactionItem>
-        <S.ReactionItem>
-          {/* <S.ReactionIcon src={commentIcon} alt="댓글" /> */}
-          <S.ReactionCount>{comments}</S.ReactionCount>
-        </S.ReactionItem>
-      </S.ReactionInfo>
+      <S.LeftSection>
+        <S.ProgressCircle progress={progressPercentage}>
+          <S.CharacterWrapper>
+            <Character id="0" />
+          </S.CharacterWrapper>
+        </S.ProgressCircle>
+        <S.GoalText>
+          <span>
+            목표: ₩{target.toLocaleString()} ({progressPercentage}%)
+          </span>
+        </S.GoalText>
+      </S.LeftSection>
+      <S.RightSection>
+        <S.AuthorInfo>
+          <S.Author>{author}</S.Author>
+          <S.Period>{period}</S.Period>
+        </S.AuthorInfo>
+        <S.Title>{title}</S.Title>
+        <S.Content>{content}</S.Content>
+
+        <S.ReactionInfo>
+          <S.ReactionItem>
+            {/* <S.ReactionIcon src={heartIcon} alt="좋아요" /> */}
+            <S.ReactionCount>좋아요 {likes}</S.ReactionCount>
+          </S.ReactionItem>
+          <S.ReactionItem>
+            {/* <S.ReactionIcon src={commentIcon} alt="댓글" /> */}
+            <S.ReactionCount>댓글 {comments}</S.ReactionCount>
+          </S.ReactionItem>
+        </S.ReactionInfo>
+      </S.RightSection>
     </S.Container>
   );
 };
