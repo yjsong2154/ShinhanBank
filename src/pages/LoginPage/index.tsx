437aee91f863e240b2acb731d2da7acde061666e
yjsong2154 <yjsong2153@gmail.com>
2025-08-21 13:20:22 +0900

    Fix : login build bug fix

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,4 +1,5 @@
-import { FormEvent, useState } from "react";
+import type { FormEvent } from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import * as S from "./LoginPage.styles";
 
