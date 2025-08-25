abfd28a675ccd5c9afba73fce27a5dc78b09d917
조예림 <144078388+YeRimmm-Cho@users.noreply.github.com>
2025-08-25 09:35:44 +0900

    Fix: prevent footer overlap on IOS & apply mobile-safe viewport (#6)
    * Style(main page): challenge-card UI polishing
    
    * Fix(ui): prevent footer overlap on IOS & apply mobile-safe viewport

--- src/components/Footer/Footer.styles.ts
+++ src/components/Footer/Footer.styles.ts
@@ -1,18 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
+  position: fixed;
+  z-index: 1000;
+
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 0;
+  width: 100%;
+  max-width: 500px;
+
   display: flex;
   justify-content: space-around;
   align-items: center;
-  position: fixed;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 60px;
-  background-color: white;
+
+  box-sizing: border-box;
+  height: calc(60px + env(safe-area-inset-bottom)); /* iOS 홈바 영역 */
+  padding-bottom: env(safe-area-inset-bottom);
+
+  background-color: #fff;
   border-top: 1px solid #e0e0e0;
-  max-width: 500px;
-  margin: 0 auto;
 `;
 
 export const FooterItem = styled.div`
