5e892520073b07d78bda8b211c656cd475040fb5
YeRimmm-Cho <rim000514@gmail.com>
2025-08-30 12:20:25 +0900

    Style: padding polishing

--- src/pages/NotificationPage/NotificationPage.styles.ts
+++ src/pages/NotificationPage/NotificationPage.styles.ts
@@ -1,14 +1,25 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 export const Container = styled.div`
-  padding: 20px 10px;
+  padding: 20px 20px;
   max-width: 500px;
   margin: 0 auto;
+
+  /* 배경 그라디언트 */
+  background: linear-gradient(
+    135deg,
+    rgba(97, 104, 251, 0.44) 0%,
+    rgba(145, 105, 240, 0.45) 50%,
+    rgba(200, 97, 229, 0.46) 100%
+  );
+
+  min-height: 100vh;
+  box-sizing: border-box;
 `;
 
 export const Header = styled.header`
   display: flex;
-  justify-content: space-between;
+  gap: 8px;
   align-items: center;
   margin-bottom: 20px;
 `;
@@ -19,5 +30,5 @@ export const Title = styled.h1`
 `;
 
 export const NotificationList = styled.div`
-  padding: 15px;
+  padding: 0px;
 `;
